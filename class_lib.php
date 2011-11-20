9a3df15ae4f7432b381155e4db3f1b1712acd371
Cory Koch <yunusabd786@gmail.com>
2011-11-20 12:38:03 -0500

    first commit

---
+++ class_lib.php
@@ -0,0 +1,14 @@
+<?php
+	class person {
+		var $name;//"properties"
+			
+			//Methods
+			function set_name($new_name) {
+				$this->name = $new_name;
+			}
+			function get_name() {
+				return $this->name;
+			}
+			
+	}
+?>
